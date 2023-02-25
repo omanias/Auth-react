61d639a7720f6283a23183df4a32da5038ea22fc
omanias <ojmanias1985@gmail.com>
2023-02-24 22:38:52 -0300

    unique commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Home from "./page/Home";
+import Signup from "./page/Signup";
+import Login from "./page/Login";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <section>
+          <Routes>
+            {" "}
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </section>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDted5j3AawK1n3liRtzp_9EN0IWpB63A0",
+  authDomain: "auth-react-127a8.firebaseapp.com",
+  projectId: "auth-react-127a8",
+  storageBucket: "auth-react-127a8.appspot.com",
+  messagingSenderId: "1088985509197",
+  appId: "1:1088985509197:web:01e2bbe61203dbec695b04",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export default app;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
